53d2d8bfced91d1695c12cd48b2f11f91b4a324a
Aleksandr Skirnevskyi <sanya-sha2016@ukr.net>
2021-03-16 20:46:28 +0200

    init:answer

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+  if(!Array.isArray(members)) return false;
+  let name = "";
+  members.filter((elem)=>{
+    if(typeof elem == "string"){return elem}
+   }).map((elem)=> {
+     elem = elem.trim()
+     name = `${name}${elem[0]}`
+   })
+   name = name.toUpperCase()
+   return name.split('').sort().join('')
+ }
--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date ) {
+  if(typeof date != 'object')
+    return 'Unable to determine the time of year!'
+
+  if(toString.call(date) != '[object Date]') throw 'It is not a date!';
+  let arrMon = ['winter', 'winter' , 'spring', 'spring', 'spring', 'summer', 'summer', 'summer', 'autumn', 'autumn', 'autumn', 'winter']
+  let mon = date.getMonth()
+
+  for(let i = 0; i < arrMon.length; i++) {
+    if(mon == i) {
+      return arrMon[i]
+    }
+  }
 };
